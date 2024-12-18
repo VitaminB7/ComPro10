236d1a73418eb7013901071311975096bb58a993
ZiV99 <71887602+VitaminB7@users.noreply.github.com>
2024-12-18 13:02:30 +0700

    lab10_3.cpp Completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,34 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    ifstream source;
+    string a;
+    double sum=0,std=0,mean,sd;
+    int i=0;
+    source.open("score.txt");
+    
+    while(getline(source,a)){
+    
+    sum+=stod(a);    
+    
+    std+= pow(stod(a),2);
+        
+    i++;
+    }
+    source.close();
+
+    
+    cout << "Number of data = "<<i<<'\n';
+    cout << setprecision(3);
+    mean = sum/i;
+    sd = (std/i)-pow(mean,2);
+    cout << "Mean = "<<mean<<'\n';
+    cout << "Standard deviation = "<<sqrt(sd);
+    
+    
+    
+}
